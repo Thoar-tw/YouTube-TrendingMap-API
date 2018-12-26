b970e2ae8173279a6f15022057ea1e3bde8574df
happyjohnnylin <happyjohnnylin@gmail.com>
2018-12-26 15:47:42 +0800

    add representers in presentation layer

--- app/presentation/representers/continent_top_videos_list_representer.rb
+++ app/presentation/representers/continent_top_videos_list_representer.rb
@@ -3,16 +3,15 @@
 require 'roar/decorator'
 require 'roar/json'
 
-require_relative 'country_top_videos_list_representer'
-
-module CodePraise
+module YouTubeTrendingMap
   module Representer
     # Represents folder summary about repo's folder
     class ContinentTopVideosList < Roar::Decorator
       include Roar::JSON
+
       property :type
       property :count
-      collection :videos, extend: Representer::CountryTopVideosList, class: OpenStruct
+      collection :videos, extend: Representer::TopVideo, class: OpenStruct
     end
   end
 end
--- app/presentation/representers/country_top_videos_list_representer.rb
+++ app/presentation/representers/country_top_videos_list_representer.rb
@@ -3,14 +3,15 @@
 require 'roar/decorator'
 require 'roar/json'
 
-module CodePraise
+module YouTubeTrendingMap
   module Representer
     # Represents folder summary about repo's folder
     class CountryTopVideosList < Roar::Decorator
       include Roar::JSON
+
       property :type
       property :count
-      collection :videos, extend: Representer::ContinentTopVideosList, class: OpenStruct
+      collection :videos, extend: Representer::TopVideo, class: OpenStruct
     end
   end
 end
