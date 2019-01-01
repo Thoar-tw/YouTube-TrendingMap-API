8674a7cbce96c321b8ff680e005432139efe9d16
happyjohnnylin <happyjohnnylin@gmail.com>
2019-01-01 13:45:40 +0800

    add service objects for favorite videos

---
+++ app/application/services/list_favorite_videos.rb
@@ -0,0 +1,32 @@
+# frozen_string_literal: true
+
+require 'dry/transaction'
+
+module YouTubeTrendingMap
+  module Services
+    # Retrieves array of all listed project entities
+    class ListFavoriteVideos
+      include Dry::Transaction
+
+      step :retrieve_videos
+
+      private
+
+      def retrieve_videos # rubocop:disable Metrics/MethodLength
+        FavoriteVideosRepository::For
+          .klass(Entity::FavoriteVideo).all
+          .yield_self { |videos| Value::VideosList.new(videos) }
+          .yield_self do |list|
+            Success(Value::Result.new(status: :ok, message: list))
+          end
+      rescue StandardError
+        Failure(
+          Value::Result.new(
+            status: :internal_error,
+            message: 'Cannot access database'
+          )
+        )
+      end
+    end
+  end
+end
