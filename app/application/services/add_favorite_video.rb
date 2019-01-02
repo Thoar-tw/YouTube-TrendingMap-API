cded1947ccf46a65a0db1ded1b8e1787e7c811a2
happyjohnnylin <happyjohnnylin@gmail.com>
2019-01-02 18:13:40 +0800

    modify and fix Services::AddFavoriteVideo

--- app/application/services/add_favorite_video.rb
+++ app/application/services/add_favorite_video.rb
@@ -12,31 +12,40 @@ module YouTubeTrendingMap
       private
 
       def validate_input(input)
-        if !(input[:origin_id].nil? || input[:title].nil? || 
+        if !(input[:origin_id].nil? || input[:title].nil? ||
           input[:channel_title].nil? || input[:view_count].nil? ||
           input[:embed_link].nil?)
+
+          input[:video_entity] = build_entity(input)
           Success(input)
         else
-          puts 'Failure: nil'
-          Failure('Some inputs are nil!')
+          Failure(
+            Value::Result.new(
+              status: :bad_request,
+              message: 'Some inputs are nil!'
+            )
+          )
         end
       end
 
-      def store_video(input) # rubocop:disable Metrics/MethodLength
-        video = Entity::FavoriteVideo.new(
+      def store_video(input)
+        if (video = input[:video_entity])
+          result = FavoriteVideosRepository::For.entity(video).find_or_create(video)
+          Success(Value::Result.new(status: :created, message: result))
+        end
+      rescue StandardError => error
+        puts error.backtrace.join("\n")
+        Failure('Having trouble accessing the database')
+      end
+
+      def build_entity(input)
+        Entity::FavoriteVideo.new(
           origin_id: input[:origin_id],
           title: input[:title],
           channel_title: input[:channel_title],
           view_count: input[:view_count],
           embed_link: input[:embed_link]
         )
-
-        video_stored = FavoriteVideosRepository::For.entity(video).find_or_create(video)
-
-        Success(video_stored)
-      rescue StandardError => error
-        puts error.backtrace.join("\n")
-        Failure('Having trouble accessing the database')
       end
     end
   end
