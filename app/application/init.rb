c67ac182fa6da038e13e60a1d9454f4f32d6cd67
happyjohnnylin <happyjohnnylin@gmail.com>
2019-01-05 17:32:52 +0800

    change the order of init for safe

--- app/application/init.rb
+++ app/application/init.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-folders = %w[controllers services values]
+folders = %w[values services controllers]
 folders.each do |folder|
   require_relative "#{folder}/init.rb"
 end
