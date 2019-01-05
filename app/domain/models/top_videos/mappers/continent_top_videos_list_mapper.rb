93989c98f40b52f4a2ad0d798cc1b90251f57e64
happyjohnnylin <happyjohnnylin@gmail.com>
2019-01-05 17:33:42 +0800

    change .each to .map

--- app/domain/models/top_videos/mappers/continent_top_videos_list_mapper.rb
+++ app/domain/models/top_videos/mappers/continent_top_videos_list_mapper.rb
@@ -1,5 +1,9 @@
 # frozen_string_literal: true
 
+CONTINENT_COUNTRY_CODES = JSON.parse(
+  File.read('config/continent_country_codes.json')
+)
+
 module YouTubeTrendingMap
   module Mapper
     # Data structure of trending list queried from Youtube
@@ -47,24 +51,18 @@ module YouTubeTrendingMap
           end
 
         # country_codes = country_codes_in_(continent)
-        list = []
-        country_codes.each do |country_code|
-          list << Mapper::CountryTopVideosList
-                  .new(@api_key, @gateway_class)
-                  .get(country_code, category_id, max_results)
+        country_codes.map do |country_code|
+          Mapper::CountryTopVideosList
+            .new(@api_key, @gateway_class)
+            .get(country_code, category_id, max_results)
         end
-
-        list
       end
       # rubocop:enable Metrics/MethodLength
 
       def country_codes_in_(continent)
-        country_codes = []
-        CONTINENT_COUNTRY_CODES[continent].each do |country_code, _|
-          country_codes << country_code
+        CONTINENT_COUNTRY_CODES[continent].map do |country_code, _|
+          country_code
         end
-
-        country_codes
       end
     end
   end
